b63169cba4515d306a9f10eb3eb8e863228a87d3
ElonMax <cappukan@gmail.com>
2024-01-13 02:22:09 +0300

    adds function for download from hf

--- src/bots/download/from_huggingface.py
+++ src/bots/download/from_huggingface.py
@@ -1,16 +1,32 @@
 import pathlib
 
-from huggingface_hub import snapshot_download
+from huggingface_hub import snapshot_download, hf_hub_download
 
 
 project_path = pathlib.Path(__file__).parents[3]
+
 models_path = project_path.joinpath("models")
 models_path.mkdir(exist_ok=True, parents=True)
 
+data_path = project_path.joinpath("data/raw")
+data_path.mkdir(exist_ok=True, parents=True)
 
-mistral_path = models_path.joinpath("Mistral-7B-Instruct-v0.2")
-snapshot_download(
-    repo_id="mistralai/Mistral-7B-Instruct-v0.2",
-    local_dir=mistral_path,
-    ignore_patterns=["pytorch_model.bin.index.json", "*.bin", ".gitattributes"]
-)
+
+def download():
+    mistral_path = models_path.joinpath("Mistral-7B-Instruct-v0.2")
+    snapshot_download(
+        repo_id="mistralai/Mistral-7B-Instruct-v0.2",
+        local_dir=mistral_path,
+        ignore_patterns=["pytorch_model.bin.index.json", "*.bin", ".gitattributes"]
+    )
+
+    hf_hub_download(
+        repo_id="lksy/ru_instruct_gpt4",
+        repo_type="dataset",
+        local_dir=data_path,
+        filename="ru_instruct_gpt4.jsonl"
+    )
+
+
+if __name__ == '__main__':
+    download()
