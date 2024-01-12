3dd43f90dc1d722fd24cda4263f49291589dca41
ElonMax <cappukan@gmail.com>
2024-01-13 02:09:08 +0300

    adds function for gpu check

--- src/bots/gpu_check.py
+++ src/bots/gpu_check.py
@@ -1,11 +1,16 @@
 import torch
 
-stat = {
-    "cuda": torch.cuda.is_available(),
-    "version": torch.version.cuda,
-    "device_count": torch.cuda.device_count(),
-    "current": torch.cuda.current_device(),
-    "name": [torch.cuda.get_device_name(name) for name in range(torch.cuda.device_count())]
-}
 
-print(stat)
+def get_gpu_status():
+    return {
+        "cuda": torch.cuda.is_available(),
+        "version": torch.version.cuda,
+        "device_count": torch.cuda.device_count(),
+        "current": torch.cuda.current_device(),
+        "name": [torch.cuda.get_device_name(name) for name in range(torch.cuda.device_count())]
+    }
+
+
+if __name__ == '__main__':
+    stat = get_gpu_status()
+    print(stat)
