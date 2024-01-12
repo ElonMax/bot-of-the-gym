b9178c6b5d4930af74888da40405080d3a37b53c
ElonMax <cappukan@gmail.com>
2024-01-12 13:16:41 +0300

    delete tokenizer.py

--- src/bots/data/tokenizer.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoTokenizer
-
-
-pretrained_dir = "/s/ls4/groups/g0126/transformers_models/mistralai/Mistral-7B-Instruct-v0.2/"
-
-
-tokenizer = AutoTokenizer.from_pretrained(pretrained_dir)
-
