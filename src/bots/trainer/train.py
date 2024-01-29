99b75f932b70cf805b853e459d70169a9188f476
ElonMax <cappukan@gmail.com>
2024-01-29 16:33:21 +0300

    add profiler with tensorboard to train script

--- src/bots/trainer/train.py
+++ src/bots/trainer/train.py
@@ -1,5 +1,6 @@
-import time
+import pathlib
 import random
+import datetime
 
 import torch
 import torch.distributed
@@ -7,12 +8,27 @@ import numpy as np
 
 from torch.utils.data import DataLoader
 from torch.utils.data.distributed import DistributedSampler
+from torch.utils.tensorboard import SummaryWriter
 
 from bots.data.dataset import BotDataset
 
 
 def train(train_data, model, optimizer, config, tokenizer, device):
-    # model.to(device)
+    prof = torch.profiler.profile(
+        activities=[torch.profiler.ProfilerActivity.CPU, torch.profiler.ProfilerActivity.CUDA],
+        on_trace_ready=torch.profiler.tensorboard_trace_handler(config["logdir"]),
+        record_shapes=True,
+        profile_memory=True,
+        use_cuda=True
+    )
+
+    log_path = pathlib.Path(config["logdir"]) / datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+
+    writer = SummaryWriter(log_path.__str__())
+
+    metrics = {
+        "Loss/train": None
+    }
 
     # reproducibility
     seed = config["seed"]
@@ -27,21 +43,15 @@ def train(train_data, model, optimizer, config, tokenizer, device):
     sampler = DistributedSampler(train_dataset)
 
     train_dataloader = DataLoader(dataset=train_dataset, sampler=sampler, **config["train_loader"])
-    # train_dataloader = DataLoader(dataset=train_dataset, **config["train_loader"])
-
-    # set optimizer
-    # optimizer = torch.optim.Adam(params=model.parameters(), **config["optimizer"])
 
     max_epochs = config["train_epochs"]
+    prof.start()
     for epoch in range(max_epochs):
-
+        prof.step()
         model.train()
-
         torch.distributed.barrier()
 
-        avg_time = []
         for step, data in enumerate(train_dataloader, 1):
-            start_time = time.time()
             input_ids = data["input_ids"].to(device)
             attn_mask = data["attention_mask"].to(device)
             labels = data["labels"].to(device)
@@ -55,22 +65,25 @@ def train(train_data, model, optimizer, config, tokenizer, device):
 
             loss = outputs[0]
 
+            writer.add_scalar("Loss/train", loss, step)
+
             # if step % 10 == 0:
-            print("Epoch: {}/{}\t Step: {}/{}\t Loss: {:.3f}\t Avg Time: {:.5f}\t Device: {}".format(epoch+1,
-                                                                                                     max_epochs,
-                                                                                                     step,
-                                                                                                     len(train_dataloader),
-                                                                                                     loss.item(),
-                                                                                                     sum(avg_time)/(step),
-                                                                                                     device))
+            print("Epoch: {}/{}\t Step: {}/{}\t Loss: {:.3f}\t".format(epoch+1,
+                                                                       max_epochs,
+                                                                       step,
+                                                                       len(train_dataloader),
+                                                                       loss.item()))
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-            avg_time.append(time.time() - start_time)
-
         if device == 0:
             model.module.save_pretrained(config["save_path"])
+
+    prof.stop()
+    writer.add_hparams(hparam_dict=config, metric_dict=metrics)
+    writer.flush()
+    writer.close()
 
     return model, tokenizer
