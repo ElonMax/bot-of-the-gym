0404d0a6edf89a84c0525bf8d92519184fcc8f65
ElonMax <cappukan@gmail.com>
2024-01-30 19:41:24 +0300

    add eval stage and mlflow logger

--- src/bots/trainer/train.py
+++ src/bots/trainer/train.py
@@ -1,89 +1,112 @@
-import pathlib
-import random
 import datetime
+import random
 
+import mlflow
 import torch
 import torch.distributed
 import numpy as np
 
 from torch.utils.data import DataLoader
 from torch.utils.data.distributed import DistributedSampler
-from torch.utils.tensorboard import SummaryWriter
 
 from bots.data.dataset import BotDataset
 
 
-def train(train_data, model, optimizer, config, tokenizer, device):
-    log_path = pathlib.Path(config["logdir"]) / datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+def train(train_data, valid_data, model, optimizer, config, tokenizer, device):
 
-    prof = torch.profiler.profile(
-        activities=[torch.profiler.ProfilerActivity.CPU, torch.profiler.ProfilerActivity.CUDA],
-        on_trace_ready=torch.profiler.tensorboard_trace_handler(log_path.__str__()),
-        record_shapes=True,
-        profile_memory=True,
-        use_cuda=True
-    )
+    torch.distributed.barrier()
+    time = datetime.datetime.now().strftime("%d%m%Y-%H%M")
 
-    writer = SummaryWriter(log_path.__str__())
+    mlflow.set_tracking_uri(config["log_path"])
 
-    metrics = {
-        "Loss/train": None
-    }
+    if device == 0:
+        print("Create experiment...")
 
-    # reproducibility
-    seed = config["seed"]
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
+        try:
+            mlflow.create_experiment(config["experiment_name"])
+        except mlflow.exceptions.MlflowException as e:
+            print(e)
 
-    # load data
-    train_dataset = BotDataset(dataframe=train_data, tokenizer=tokenizer, text_length=config["max_tokens"])
+    torch.distributed.barrier()
+    mlflow.set_experiment(config["experiment_name"])
 
-    sampler = DistributedSampler(train_dataset)
+    run_name = "{}-rank:{}".format(time, device)
+    with mlflow.start_run(run_name=run_name):
+        mlflow.log_params(config)
 
-    train_dataloader = DataLoader(dataset=train_dataset, sampler=sampler, **config["train_loader"])
+        # reproducibility
+        seed = config["seed"]
+        random.seed(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        torch.backends.cudnn.deterministic = True
 
-    max_epochs = config["train_epochs"]
-    prof.start()
-    for epoch in range(max_epochs):
-        prof.step()
-        model.train()
-        torch.distributed.barrier()
+        # load data
+        train_dataset = BotDataset(dataframe=train_data, tokenizer=tokenizer, text_length=config["max_tokens"])
+        valid_dataset = BotDataset(dataframe=valid_data, tokenizer=tokenizer, text_length=config["max_tokens"])
 
-        for step, data in enumerate(train_dataloader, 1):
-            input_ids = data["input_ids"].to(device)
-            attn_mask = data["attention_mask"].to(device)
-            labels = data["labels"].to(device)
-            labels[labels == tokenizer.pad_token_id] = -100
+        train_sampler = DistributedSampler(train_dataset)
+        valid_sampler = DistributedSampler(valid_dataset)
 
-            outputs = model(
-                input_ids=input_ids,
-                attention_mask=attn_mask,
-                labels=labels
-            )
+        train_dataloader = DataLoader(dataset=train_dataset, sampler=train_sampler, **config["train_loader"])
+        valid_dataloader = DataLoader(dataset=valid_dataset, sampler=valid_sampler, **config["valid_loader"])
 
-            loss = outputs[0]
+        max_epochs = config["train_epochs"]
+        for epoch in range(max_epochs):
+            model.train()
+            torch.distributed.barrier()
 
-            writer.add_scalar("Loss/train", loss, step)
+            for step, data in enumerate(train_dataloader, 1):
+                input_ids = data["input_ids"].to(device)
+                attn_mask = data["attention_mask"].to(device)
+                labels = data["labels"].to(device)
 
-            # if step % 10 == 0:
-            print("Epoch: {}/{}\t Step: {}/{}\t Loss: {:.3f}\t".format(epoch+1,
-                                                                       max_epochs,
-                                                                       step,
-                                                                       len(train_dataloader),
-                                                                       loss.item()))
+                outputs = model(
+                    input_ids=input_ids,
+                    attention_mask=attn_mask,
+                    labels=labels
+                )
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+                loss = outputs[0]
 
-        if device == 0:
-            model.module.save_pretrained(config["save_path"])
+                print("Train Epoch: {}/{}\t Step: {}/{}\t Loss: {:.3f}\t".format(epoch+1,
+                                                                                 max_epochs,
+                                                                                 step,
+                                                                                 len(train_dataloader),
+                                                                                 loss.item()))
 
-    prof.stop()
-    writer.add_hparams(hparam_dict=config, metric_dict=metrics)
-    writer.flush()
-    writer.close()
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                mlflow.log_metric("Loss/train", loss, step)
+
+            model.eval()
+            with torch.no_grad():
+                eval_losses = []
+                for step, data in enumerate(valid_dataloader, 1):
+                    input_ids = data["input_ids"].to(device)
+                    attn_mask = data["attention_mask"].to(device)
+                    labels = data["labels"].to(device)
+
+                    outputs = model(
+                        input_ids=input_ids,
+                        attention_mask=attn_mask,
+                        labels=labels
+                    )
+
+                    print("Eval Step: {}/{}".format(step, len(valid_dataloader)))
+
+                    eval_losses.append(outputs[0])
+
+                loss1 = sum(eval_losses) / len(eval_losses)
+
+                torch.distributed.barrier()
+                torch.distributed.all_reduce(loss1, op=torch.distributed.ReduceOp.AVG)
+                if device == 0:
+                    mlflow.log_metric("Loss/eval", loss1, epoch)
+
+            if device == 0:
+                model.module.save_pretrained(config["save_path"])
 
     return model, tokenizer
