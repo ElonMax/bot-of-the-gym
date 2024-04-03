27eb3c83b0349a9e051ff1a96483a6f1b6fc56c2
ElonMax <cappukan@gmail.com>
2024-04-03 15:53:56 +0300

    fix description in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="bots",
     version="0.0.1",
-    keywords="language models, chatbot, instruct. fine-tuning",
+    keywords="language models, chatbot, instruct, fine-tuning",
     url="https://github.com/ElonMax/bot-of-the-gym",
     description="Instruments for tuning langauge models",
     packages=find_packages(
